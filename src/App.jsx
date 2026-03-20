f6ce5c7649a0c5908d6d6aca1038b6815c969355
Balakumar <balakumarn1025@gmail.com>
2026-03-21 03:06:50 +0530

    Fix import path casing for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route,  } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./Components/NavBar";
-import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Footer from "./Components/Footer";
 import Education from "./pages/Education";
@@ -8,11 +7,9 @@ import Skills from "./Pages/Skills";
 import Projects from "./Pages/Projects";
 import Contact from "./Contact";
 import Download_resume from "./pages/Download_resume";
-
+import Home from "./pages/Home";
 
 function App() {
-  
-
   // // using useNavigate to navigate to different pages
   // const navigate=useNavigate();
   return (
@@ -27,7 +24,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/download_resume" element={<Download_resume />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
