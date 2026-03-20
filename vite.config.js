59cd75cd520bb9bd04ed236159096f0f4a32eb53
Balakumar <balakumarn1025@gmail.com>
2026-03-21 02:44:01 +0530

    fix base for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Portfolio/'   // 👈 your repository name
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  plugins: [react()]
 })
